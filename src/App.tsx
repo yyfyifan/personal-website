52e5afa7de757a15a1ca62f15f7b2efa7d72e8ff
yyfyifan <yyfyifan@gmail.com>
2023-06-03 00:16:17 -0700

    Install tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div>Personal Website</div>;
+  return <div className="text-3xl font-bold underline">Personal Website</div>;
 }
 
 export default App;
