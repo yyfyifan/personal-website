096d99f7cb4ee54dfe90552d8b1aa6d189e892bc
yyfyifan <yyfyifan@gmail.com>
2023-06-12 19:22:21 -0700

    Adjsut the gap between the intro and typings

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -11,18 +11,20 @@ const Home = () => {
         className="max-w-5xl mx-auto px-8 h-full
                 font-roboto
                 flex flex-col justify-center">
-        <Avatar />
-        <p className="text-xl text-teal-600 dark:text-red-300 font-light">👋 Hi, my name is</p>
-        <h1 className="text-3xl sm:text-5xl text-teal-600 font-bold py-5">
-          <span className="dark:text-white">Yifan</span>
-          &nbsp;
-          <span className="dark:text-pinkred">Yang</span>
-        </h1>
-        <h2
-          className="text-xl sm:text-2xl text-teal-600 dark:text-white 
+        <div className="mb-20">
+          <Avatar />
+          <p className="text-xl text-teal-600 dark:text-red-300 font-light">👋 Hi, my name is</p>
+          <h1 className="text-3xl sm:text-5xl text-teal-600 font-bold py-5">
+            <span className="dark:text-white">Yifan</span>
+            &nbsp;
+            <span className="dark:text-pinkred">Yang</span>
+          </h1>
+          <h2
+            className="text-xl sm:text-2xl text-teal-600 dark:text-white 
             tracking-widest font-light mb-10">
-          I'm a Software Engineer.
-        </h2>
+            I'm a Software Engineer.
+          </h2>
+        </div>
         <TerminalWindow>
           <TypedGreetings />
         </TerminalWindow>
