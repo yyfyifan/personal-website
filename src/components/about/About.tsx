76bba8c967dbb9df58823077649fe0840a28c379
yyfyifan <yyfyifan@gmail.com>
2023-06-16 22:16:18 -0700

    Adjust the styles of the About component in small screen

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -1,18 +1,29 @@
+import classNames from "classnames";
 const About = () => {
   return (
     <section
       id="about"
-      className="h-screen text-teal-600 dark:text-gray-200 flex flex-col justify-center items-center snap-start">
-      <h2 className="text-5xl text-center mb-16 border-b-4 border-pinkred">About Me</h2>
-      <div className="max-w-4xl grid sm:grid-cols-2 gap-8">
-        <div className="sm:text-right text-2xl font-bold">
+      className={classNames(
+        "h-screen text-teal-600 dark:text-gray-200",
+        "flex flex-col justify-center items-center snap-start",
+        "px-10"
+      )}>
+      <h2
+        className={classNames(
+          "text-2xl sm:text-5xl text-center",
+          "mb-4 sm:mb-16 border-b-4 border-pinkred"
+        )}>
+        About Me
+      </h2>
+      <div className="max-w-4xl grid grid-cols-1 sm:grid-cols-2 gap-3 sm:gap-8">
+        <div className="text-md sm:text-2xl sm:text-right font-bold">
           <p>Hey there!</p>
           <p>
             I'm an experienced Software Engineer who's all about diving into the exciting world of
             technology.
           </p>
         </div>
-        <div>
+        <div className="text-sm sm:text-xl">
           <p className="mb-4">
             I am driven by a passion for problem-solving and a desire to create innovative solutions
             that make a positive impact. With a strong foundation in programming languages and
