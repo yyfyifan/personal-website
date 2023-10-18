aa4e06ded35848875fea3d20438846daae01b433
yyfyifan <yyfyifan@gmail.com>
2023-10-17 19:39:44 -0700

    Replace 'email' with 'Goodreads' on the side navigation links

--- src/components/navbar/SocialIcons.tsx
+++ src/components/navbar/SocialIcons.tsx
@@ -1,50 +1,60 @@
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { HiOutlineMail } from "react-icons/hi";
+import { FaGithub, FaLinkedin, FaGoodreads } from "react-icons/fa";
 import { BsFillPersonLinesFill } from "react-icons/bs";
+import classNames from "classnames";
+
+const socialIconProperties = [
+  {
+    name: "LinkedIn",
+    icon: <FaLinkedin size={30} />,
+    href: "https://www.linkedin.com/in/yifanyang-swe/",
+    backgroundColor: "bg-[#0a66c2]",
+  },
+  {
+    name: "Github",
+    icon: <FaGithub size={30} />,
+    href: "https://github.com/yyfyifan",
+    backgroundColor: "bg-[#24292f]",
+  },
+  {
+    name: "Goodreads",
+    icon: <FaGoodreads size={30} />,
+    href: "https://www.goodreads.com/yifanyang/",
+    backgroundColor: "bg-orange-400",
+  },
+  {
+    name: "Resume",
+    icon: <BsFillPersonLinesFill size={30} />,
+    href: "https://bit.ly/yifan-yang-resume",
+    backgroundColor: "bg-red-500",
+  },
+  // {
+  //   name: "Email",
+  //   icon: <HiOutlineMail size={30} />,
+  //   href: "mailto:yifan.yang.swe@gmail.com",
+  //   backgroundColor: "bg-red-500",
+  // },
+];
 
 const SocialIcons = () => {
   return (
     // Only display in large screen
     <ul className="fixed hidden lg:flex flex-col left-0 top-[40%] text-xl text-gray-200">
-      <li
-        className="w-[160px] h-[60px] flex items-center bg-[#0a66c2]
-            ml-[-100px] hover:ml-0 duration-300 ">
-        <a
-          href="https://www.linkedin.com/in/yifanyang-swe/"
-          target="_blank"
-          className="w-full p-4 flex justify-between items-center">
-          Linkedin <FaLinkedin size={30} />
-        </a>
-      </li>
-      <li
-        className="w-[160px] h-[60px] flex items-center bg-[#24292f]
-            ml-[-100px] hover:ml-0 duration-300 ">
-        <a
-          href="https://github.com/yyfyifan"
-          target="_blank"
-          className="w-full p-4 flex justify-between items-center">
-          Github <FaGithub size={30} />
-        </a>
-      </li>
-      <li
-        className="w-[160px] h-[60px] flex items-center bg-red-500
-            ml-[-100px] hover:ml-0 duration-300 ">
-        <a
-          href="mailto:yifan.yang.swe@gmail.com"
-          className="w-full p-4 flex justify-between items-center">
-          Email <HiOutlineMail size={30} />
-        </a>
-      </li>
-      <li
-        className="w-[160px] h-[60px] flex items-center bg-orange-400
-            ml-[-100px] hover:ml-0 duration-300 ">
-        <a
-          href="https://bit.ly/yifan-yang-resume"
-          target="_blank"
-          className="w-full p-4 flex justify-between items-center">
-          Resume <BsFillPersonLinesFill size={30} />
-        </a>
-      </li>
+      {socialIconProperties.map((props) => (
+        <li
+          key={props.name}
+          className={classNames(
+            "w-[180px] h-[60px] flex items-center ml-[-120px] hover:ml-0 duration-300",
+            props.backgroundColor
+          )}>
+          <a
+            href={props.href}
+            target="_blank"
+            className="w-full p-4 flex justify-between items-center">
+            {props.name}
+            <span className="">{props.icon}</span>
+          </a>
+        </li>
+      ))}
     </ul>
   );
 };
